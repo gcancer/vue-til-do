1dcab6d04dc92ca3dffd55c463a78ace1a2735d6
kaper-jiyong <kaper@aipers.com>
2021-02-15 17:01:38 +0900

    Login 페이지 개발

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input id="password" type="text" v-model="password" />
+    </div>
+    <button :disabled="!isUsernameValid || !password" type="submit">
+      로그인
+    </button>
+    <p>{{ logMessage }}</p>
+  </form>
+</template>
+
+<script>
+import { loginUser } from '@/api/index.js';
+import { validateEmail } from '@/utils/validation.js';
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      logMessage: '',
+    };
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
+  methods: {
+    async submitForm() {
+      try {
+        // 비즈니스 로직
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        const { data } = await loginUser(userData);
+        this.logMessage = `${data.user.username}님 환영합니다`;
+      } catch (error) {
+        // 에러 핸들링할 코드
+        // error메시지를 화면에 노출시키는게 좋은 해결책 중 하나
+        console.log(error.response.data);
+        this.logMessage = error.response.data;
+      } finally {
+        this.initForm(); // finally로 내용 비우기
+      }
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -2,23 +2,44 @@
   <form @submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text" v-model="username" />
+      <input
+        :class="{ inputBorder: !isUsernameValid }"
+        id="username"
+        type="text"
+        v-model="username"
+      />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="text" v-model="password" />
+      <input
+        :class="{ inputBorder: !password }"
+        id="password"
+        type="text"
+        v-model="password"
+      />
     </div>
     <div>
       <label for="nickname">nickname: </label>
-      <input id="nickname" type="text" v-model="nickname" />
+      <input
+        :class="{ inputBorder: !nickname }"
+        id="nickname"
+        type="text"
+        v-model="nickname"
+      />
     </div>
-    <button type="submit">회원 가입</button>
+    <button
+      :disabled="!isUsernameValid || !password || !nickname"
+      type="submit"
+    >
+      회원 가입
+    </button>
     <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
 import { registerUser } from '@/api/index.js';
+import { validateEmail } from '@/utils/validation.js';
 export default {
   data() {
     return {
@@ -30,21 +51,31 @@ export default {
       logMessage: '',
     };
   },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
   methods: {
     // 부가 동작을 하기 위해선 메서드에 비동기 처리가 필요함
     async submitForm() {
       // button을 누르면 이벤트 버블링에 의해 form태그로 정보가 올라감 @submit으로 받아서 메서드로 전달
       // .prevent -> 제출 후에 새로고침 방지
-      const userData = {
-        username: this.username,
-        password: this.password,
-        nickname: this.nickname,
-      };
-      //const response
-      const { data } = await registerUser(userData);
-      console.log(data.username);
-      this.logMessage = `${data.username}님이 가입되었습니다`;
-      this.initForm();
+      try {
+        const userData = {
+          username: this.username,
+          password: this.password,
+          nickname: this.nickname,
+        };
+        //const response
+        const { data } = await registerUser(userData);
+        console.log(data.username);
+        this.logMessage = `${data.username}님이 가입되었습니다`;
+      } catch (error) {
+        this.logMessage = error.response.data.errmsg;
+      } finally {
+        this.initForm();
+      }
     },
     initForm() {
       // js는 type이 없기 때문에 null로 해도 상관은 없지만..
@@ -58,4 +89,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.inputBorder {
+  border: 2px solid red;
+}
+</style>
