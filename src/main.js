7c5d99791fc1324ab0a7aa95d82de3da942c51d4
kaper-jiyong <kaper@aipers.com>
2021-02-17 10:50:09 +0900

    데이터 포맷팅

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes/index.js';
 import store from '@/store/index.js';
+import { formatDate } from '@/utils/filters';
 
+Vue.filter('formatDate', formatDate); // 전역 설정
 Vue.config.productionTip = false;
 
 new Vue({
